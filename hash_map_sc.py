36811aa77a63547fe52571eaef4e573a15219471
CassKramer <107910626+CassKramer@users.noreply.github.com>
2023-03-13 18:10:10 -0700

    part 1(updated clear method)

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -134,7 +134,7 @@ class HashMap:
         """
         some_buckets = DynamicArray()
         for index in range(self._capacity):
-            some_buckets.append(None)
+            some_buckets.append(0)
         self._buckets = some_buckets
         self._size = 0
 
