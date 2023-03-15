2142a822c11a1a6254c8cbfc65ed97c92e1bdadd
CassKramer <107910626+CassKramer@users.noreply.github.com>
2023-03-14 19:07:04 -0700

    part 2 (updated get and contains_key method)

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -165,24 +165,45 @@ class HashMap:
         """
         Returns the value associated with the given key
         """
-        for index in range(self._buckets.length()):
-            num = self._buckets[index]
-            if num is not None:
-                if num.key == key and num.is_tombstone is False:
-                    return num.value
-        return None
+        probe = 1
+
+        new_hash = self._hash_function(key)
+        hash_index = new_hash % self._capacity
+        initial_index = hash_index
+
+        if self._buckets[hash_index] is not None:
+            while self._buckets[hash_index] is not None:
+                if self._buckets[hash_index].key == key:
+                    return self._buckets[hash_index].value
+                else:
+                    hash_index = (initial_index + probe ** 2) % self._capacity
+                    probe += 1
+        else:
+            return None
 
     def contains_key(self, key: str) -> bool:
         """
         If the key is in the hash map, it returns True, otherwise False
         """
-        for index in range(self._buckets.length()):
-            num = self._buckets[index]
-            if num is not None:
-                if num.key == key:
-                    return True
-        return False
 
+        probe = 1
+
+        new_hash = self._hash_function(key)
+        hash_index = new_hash % self._capacity
+        initial_index = hash_index
+
+        if self._size == 0:
+            return False
+
+        if self._buckets[hash_index] is not None and self._buckets[hash_index].is_tombstone is False:
+            while self._buckets[hash_index] is not None:
+                if self._buckets[hash_index].key == key:
+                    return True
+                else:
+                    hash_index = (initial_index + probe ** 2) % self._capacity
+                    probe += 1
+        else:
+            return False
 
     def remove(self, key: str) -> None:
         """
