f5e95aab7f8fd700cb52b641e47cf8620dc46eec
CassKramer <107910626+CassKramer@users.noreply.github.com>
2023-03-17 15:23:06 -0700

    part 2 (updated resize method)

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -163,8 +163,8 @@ class HashMap:
             if num is not None and num.is_tombstone is False:
                 self.put(num.key, num.value)
 
-        if self.table_load() > 0.5:
-            self.resize_table(self._capacity * 2)
+       # if self.table_load() > 0.5:
+           # self.resize_table(self._capacity * 2)
 
        # self._capacity = new_hash._capacity
        # self._buckets = new_hash._buckets
